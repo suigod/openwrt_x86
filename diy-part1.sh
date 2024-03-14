6fb4b6cfbe47d57bcbe1dd52d4b3ed4c3daab970
suigod <57538213+suigod@users.noreply.github.com>
2024-03-14 22:46:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,34 +13,11 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g' ./target/linux/x86/Makefile
-sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.6/g' ./target/linux/x86/Makefile
-
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 cd package/lean
 rm -rf luci-theme-argon  
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon-18.06
 git clone https://github.com/kongfl888/luci-app-adguardhome
-
-# cd lede/package
-# rm -rf ./feeds
-# rm -rf package/lean/luci-theme-argon
-# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-# git clone -b 18.06 https://github.com/kiddin9/luci-theme-edge package/lean/luci-theme-edge
-
-# sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
-
-# rm -rf package/helloworld
-# git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
-# git -C package/helloworld pull
-# rm -rf package/helloworld
-
-# rm -rf package/lean/luci-app-adguardhome
-# git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
-
-# rm -rf package/lean/luci-app-poweroff
-# git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/luci-app-poweroff
