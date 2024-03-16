6e8a47890523dbf950a66e41639bdb52ddb7403f
suigod <57538213+suigod@users.noreply.github.com>
2024-03-16 14:05:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,16 +21,16 @@ sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.6/g' ./target/linux/x86/Makef
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-cd lede/package/lean
-rm -rf ./feeds/luci/themes/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
+# cd lede/package/lean
+# rm -rf ./feeds/luci/themes/luci-theme-argon
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
 
-rm -rf ./feeds/luci/themes/luci-app-argon-config # if have
-git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git luci-app-argon-config
+# rm -rf ./feeds/luci/themes/luci-app-argon-config # if have
+# git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git luci-app-argon-config
 
 cd lede/package/lean/  
 git clone https://github.com/jerrykuku/lua-maxminddb.git  #git lua-maxminddb 依赖
-git clone https://github.com/jerrykuku/openwrt-package/luci-app-vssr.git  
+git clone https://github.com/jerrykuku/openwrt-package
 
 # git clone https://github.com/kongfl888/luci-app-adguardhome
 
